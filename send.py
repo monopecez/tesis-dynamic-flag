2ac4c0c557d5e32a5c31a33e5e499591f57d0421
Isman Junian Rahman <monopecez@gmail.com>
2016-10-24 18:02:17 +0700

    - send.py byebye base64. welcome AES.   satu blok message menjadi 32 karakter dengan padding spasi jika tidak kelipatan 32.   flag size = 3 karakter. sebelumnya 4 karakter (dari 3 karakter -> base64 -> 4 karakter)   ganti fungsi inttob64 menjadi inttoseqchar. mengubah integer -> hex -> utf string

--- send.py
+++ send.py
@@ -4,14 +4,23 @@ import random
 import time
 import base64
 import os
+from Crypto.Cipher import AES
 
 def xor_message_chunk(message):
-  n = 3
+  n = 3 #flag size (bytes)
   listnya = [int(message[i:i+n].encode('hex'),16) for i in range(0, len(message), n)]
   nextflagraw = 0
   for item in listnya:
     nextflagraw = nextflagraw ^ item
   return nextflagraw
+
+def inttoseqchar(number):
+  numberinhex = hex(number)[2:].zfill(6)
+  listnya = [chr(int(numberinhex[i:i+2],16)) for i in range(0, 6, 2)]
+  return ''.join(listnya)
+
+key = 'secretkey123456!'
+obj = AES.new(key, AES.MODE_ECB)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
@@ -31,24 +40,27 @@ nextflagraw = IV ^ int(initialflag,16)
 
 message = ' '.join(sys.argv[1:]) or "This page contains examples on basic concepts of C programming like: loops, functions, pointers, structures etc. All the examples in this page are tested and verified on GNU GCC compiler, although almost every program on in this website will work on any compiler you use. Feel free to copy the source code and execute it in your device."
 #message = "This page contains examples on basic concepts" # of C programming like: loops, functions, pointers, structures etc. All the examples in this page are tested and verified on GNU GCC compiler, although almost every program on in this website will work on any compiler you use. Feel free to copy the source code and execute it in your device."
-totalpadding = 27 - (len(message)%27)
+totalpadding = 32 - (len(message)%32)
 message = message + totalpadding*' '
+
+#encrypt first
+ciphertext = obj.encrypt(message)
 
 print("__________SENDING__________")
 
 i = 0
 
-while i != len(message)/27:
-  if i == (len(message)/27)-1:
+while i != len(ciphertext)/32:
+  if i == (len(ciphertext)/32)-1:
     nextflagraw = nextflagraw ^ int(initialflag,16)
-  itemtobesent = base64.b64encode(hex(nextflagraw)[2:].zfill(6).decode('hex') + message[(i)*27:(i+1)*27])
+  itemtobesent = inttoseqchar(nextflagraw) + ciphertext[(i)*32:(i+1)*32]
   channel.basic_publish(exchange='',
                       routing_key='firstqueue',
                       #routing_key='secondqueue',
                       body= itemtobesent,
                       properties=pika.BasicProperties(delivery_mode = 2,))
-  print("[v] %r is sent" % itemtobesent)
+  print("[v] %s is sent" % itemtobesent)
   time.sleep(random.uniform(0,1.5))
   nextflagraw = nextflagraw ^ xor_message_chunk(itemtobesent[4:])
-  body = message[(i)*27:(i+1)*27]
+  body = ciphertext[(i)*32:(i+1)*32]
   i = i + 1
