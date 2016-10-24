1f38b23c76354ee9ad9eeec7a9480e2be1af26ae
Isman Junian Rahman <monopecez@gmail.com>
2016-10-24 17:23:45 +0700

    - receive.py   tambah fungsi xor_message_chunk : me-xor-kan message dengan output integer   tambah fungsi inttob64 : mengubah integer menjadi hex, mengubah setiap byte menjadi karakter utf lalu encode ke base64   selamat tinggal flag FFFE dan FFFS, selamat datang flag dinamis.

--- receive.py
+++ receive.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import pika
 import time
+import base64
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
@@ -9,24 +10,47 @@ channel2 = connection.channel()
 channel.queue_declare(queue="firstqueue")
 channel2.queue_declare(queue="secondqueue")
 
+nextflag = ""
+nextflagint = 0
+initialflag = "FFFFFF"
+
+def xor_message_chunk(message):
+  n = 3
+  listnya = [int(message[i:i+n].encode('hex'),16) for i in range(0, len(message), n)]
+  nextflag = 0
+  for item in listnya:
+    nextflag = nextflag ^ item
+  return nextflag
+
+def inttob64(numba):
+  b64 = base64.b64encode(hex(numba)[2:].zfill(6).decode('hex'))
+  return b64
+
 def callback(ch, method, properties, body):
-  print(body,end=''),
+  global nextflag
+  global nextflagint
+  #print(body)
+  print(body,end='')
   ch.basic_ack(delivery_tag = method.delivery_tag)
-  if body.find('FFFS') != -1:
+  if (nextflag != "") and (body.find(inttob64(nextflagint))) != -1:
       channel2.basic_publish(exchange='',
                       routing_key='secondqueue',
                       body=body,
                       properties=pika.BasicProperties(delivery_mode = 2,))
-  elif body.find('FFFE') != -1:
+      #print(nextflagint)
+      #print(xor_message_chunk(body[4:]))
+      nextflagint = nextflagint ^ xor_message_chunk(body[4:])
+      #print (str(nextflag) + " ---- " + inttob64(nextflagint))
+
+  if body.find('IVIVIV') != -1:
+      nextflagint = int(body[6:]) ^ int(initialflag,16) #integer
+      nextflag = inttob64(nextflagint) #string
       channel2.basic_publish(exchange='',
                       routing_key='secondqueue',
                       body=body,
                       properties=pika.BasicProperties(delivery_mode = 2,))
-  elif body.find('IVIVIV') != -1:
-      channel2.basic_publish(exchange='',
-                      routing_key='secondqueue',
-                      body=body,
-                      properties=pika.BasicProperties(delivery_mode = 2,))
+      #print (nextflag) #string
+      #print (nextflagint) #integer
 
 
 channel.basic_consume(callback,
