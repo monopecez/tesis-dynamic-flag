b004920516bfae250ee47e7439a678cc11ff6698
Isman Junian Rahman <monopecez@gmail.com>
2016-10-24 17:52:58 +0700

    - receive.py rename variable dari nextflagint jadi nextflagraw   deteksi last flag, menentukan kapan saatnya decode. (nextflag xor firstflag = saatnya decode)

--- receive.py
+++ receive.py
@@ -11,16 +11,16 @@ channel.queue_declare(queue="firstqueue")
 channel2.queue_declare(queue="secondqueue")
 
 nextflag = ""
-nextflagint = 0
+nextflagraw = 0
 initialflag = "FFFFFF"
 
 def xor_message_chunk(message):
   n = 3
   listnya = [int(message[i:i+n].encode('hex'),16) for i in range(0, len(message), n)]
-  nextflag = 0
+  nextflagraw = 0
   for item in listnya:
-    nextflag = nextflag ^ item
-  return nextflag
+    nextflagraw = nextflagraw ^ item
+  return nextflagraw
 
 def inttob64(numba):
   b64 = base64.b64encode(hex(numba)[2:].zfill(6).decode('hex'))
@@ -28,30 +28,34 @@ def inttob64(numba):
 
 def callback(ch, method, properties, body):
   global nextflag
-  global nextflagint
+  global nextflagraw
+  #print (str(nextflagraw))
   #print(body)
-  print(body,end='')
+  print(body,end=''),
   ch.basic_ack(delivery_tag = method.delivery_tag)
-  if (nextflag != "") and (body.find(inttob64(nextflagint))) != -1:
+  if (nextflag != "") and (body.find(inttob64(nextflagraw))) != -1:
       channel2.basic_publish(exchange='',
                       routing_key='secondqueue',
                       body=body,
                       properties=pika.BasicProperties(delivery_mode = 2,))
-      #print(nextflagint)
+      #print(nextflagraw)
       #print(xor_message_chunk(body[4:]))
-      nextflagint = nextflagint ^ xor_message_chunk(body[4:])
-      #print (str(nextflag) + " ---- " + inttob64(nextflagint))
-
-  if body.find('IVIVIV') != -1:
-      nextflagint = int(body[6:]) ^ int(initialflag,16) #integer
-      nextflag = inttob64(nextflagint) #string
+      nextflagraw = nextflagraw ^ xor_message_chunk(body[4:])
+      #print (str(nextflag) + " ---- " + inttob64(nextflagraw))
+  elif body.find('IVIVIV') != -1:
+      nextflagraw = int(body[6:]) ^ int(initialflag,16) #integer
+      nextflag = inttob64(nextflagraw) #string
       channel2.basic_publish(exchange='',
                       routing_key='secondqueue',
                       body=body,
                       properties=pika.BasicProperties(delivery_mode = 2,))
       #print (nextflag) #string
-      #print (nextflagint) #integer
-
+      #print (nextflagraw) #integer
+  elif body.find(inttob64(nextflagraw ^ int(initialflag,16))) != -1:
+      channel2.basic_publish(exchange='',
+                      routing_key='secondqueue',
+                      body=body,
+                      properties=pika.BasicProperties(delivery_mode = 2,))
 
 channel.basic_consume(callback,
                       queue="firstqueue")
