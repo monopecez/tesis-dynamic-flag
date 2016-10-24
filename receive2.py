f67126f2402fa7b37db8f87f4dc5c703809de0fd
Isman Junian Rahman <monopecez@gmail.com>
2016-10-24 18:09:33 +0700

    - receive2.py hapus base64, tambah AES   gantu inttob64 jadi inttoseqchar   ganti flagsize jadi 3 karakter   tambah penghitung waktu untuk lihat kinerja

--- receive2.py
+++ receive2.py
@@ -2,43 +2,56 @@ from __future__ import print_function
 import pika
 import time
 import base64
+from Crypto.Cipher import AES
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel2 = connection.channel()
 channel2.queue_declare(queue="secondqueue")
+time.clock()
 fullbody = ''
-rawnextflag = int('ffffff',16)
+nextflagraw = int('ffffff',16)
+totaltime = 0
+key = 'secretkey123456!'
+obj = AES.new(key, AES.MODE_ECB)
+
+
 
 def xor_message_chunk(message):
   n = 3
   listnya = [int(message[i:i+n].encode('hex'),16) for i in range(0, len(message), n)]
-  nextflag = 0
+  nextflagraw = 0
   for item in listnya:
-    nextflag = nextflag ^ item
-  return nextflag
+    nextflagraw = nextflagraw ^ item
+  return nextflagraw
 
-def inttob64(numba):
-  b64 = base64.b64encode(hex(numba)[2:].zfill(6).decode('hex'))
-  return b64
+def inttoseqchar(number):
+  numberinhex = hex(number)[2:].zfill(6)
+  listnya = [chr(int(numberinhex[i:i+2],16)) for i in range(0, 6, 2)]
+  return ''.join(listnya)
 
 def callback(ch, method, properties, body):
   firstflag = "FFFFFF" 
   global fullbody
-  global rawnextflag
+  global nextflagraw
+  global totaltime
+  timenow = time.clock()
   if body[:6] == 'IVIVIV':
     print(body[6:])
-    rawnextflag = int(body[6:]) ^ int(firstflag,16)
-    print(rawnextflag)
-  elif body[:4] == inttob64(rawnextflag):
+    nextflagraw = int(body[6:]) ^ int(firstflag,16)
+    print(nextflagraw)
+  elif body[:3] == inttoseqchar(nextflagraw):
     print("Received : " + body)
-    fullbody = fullbody + body[4:]
-    rawnextflag = rawnextflag ^ xor_message_chunk(body[4:])
-  elif body[:4] == inttob64(rawnextflag ^ int(firstflag,16)):
+    fullbody = fullbody + body[3:]
+    nextflagraw = nextflagraw ^ xor_message_chunk(body[4:])
+  elif body[:3] == inttoseqchar(nextflagraw ^ int(firstflag,16)):
     print("Received : " + body)
-    fullbody = fullbody + body[4:]
-    print("Decoded  : " + base64.b64decode(fullbody))
+    fullbody = fullbody + body[3:]
+    print("Decoded  : " + obj.decrypt(fullbody))
     fullbody = ''
-  
+    print(totaltime)
+    totaltime = 0
+  totaltime = totaltime + time.clock() - timenow
+
   ch.basic_ack(delivery_tag = method.delivery_tag)
 
 channel2.basic_consume(callback,
