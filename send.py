68efea539de6ccfdf3dca77e2dafb77a2f69d7d5
Isman Junian Rahman <monopecez@gmail.com>
2016-10-24 17:36:20 +0700

    - send.py tambah fungsi xor_message_chunk juga   hitung nextflag untuk blok message selanjutnya   pesan dipecah-pecah menjadi blok dengan panjang 27 karakter per blok kemudian di-encode dengan base64 (sebelumnya diencode ke dalam base64 kemudian dipecah kedalam 32 karakter lalu kirim)

--- send.py
+++ send.py
@@ -5,13 +5,18 @@ import time
 import base64
 import os
 
+def xor_message_chunk(message):
+  n = 3
+  listnya = [int(message[i:i+n].encode('hex'),16) for i in range(0, len(message), n)]
+  nextflagraw = 0
+  for item in listnya:
+    nextflagraw = nextflagraw ^ item
+  return nextflagraw
+
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='firstqueue')
-#channel.queue_declare(queue='secondqueue')
-
-message = ' '.join(sys.argv[1:]) or "Hello World!"
 
 IV = int(os.urandom(3).encode('hex'),16)
 channel.basic_publish(exchange='',
@@ -20,35 +25,30 @@ channel.basic_publish(exchange='',
                       body= "IVIVIV" + str(IV),
                       properties=pika.BasicProperties(delivery_mode = 2,))
 print("[v] %r : IV is sent" % IV)
+initialflag = "FFFFFF"
 
-totalpadding = 32 - (len(message)%32)
-message = message + totalpadding*'F'
-message = base64.b64encode(message)
+nextflagraw = IV ^ int(initialflag,16)
+
+message = ' '.join(sys.argv[1:]) or "This page contains examples on basic concepts of C programming like: loops, functions, pointers, structures etc. All the examples in this page are tested and verified on GNU GCC compiler, although almost every program on in this website will work on any compiler you use. Feel free to copy the source code and execute it in your device."
+#message = "This page contains examples on basic concepts" # of C programming like: loops, functions, pointers, structures etc. All the examples in this page are tested and verified on GNU GCC compiler, although almost every program on in this website will work on any compiler you use. Feel free to copy the source code and execute it in your device."
+totalpadding = 27 - (len(message)%27)
+message = message + totalpadding*' '
+
+print("__________SENDING__________")
 
 i = 0
-while (i != len(message)/32):
-  time.sleep(random.uniform(0,2))
-  body = message[(i)*32:(i+1)*32]
-  if i == len(message)/32 - 1:
-    heading = "FFFE"
-  else:
-    heading = "FFFS"
+
+while i != len(message)/27:
+  if i == (len(message)/27)-1:
+    nextflagraw = nextflagraw ^ int(initialflag,16)
+  itemtobesent = base64.b64encode(hex(nextflagraw)[2:].zfill(6).decode('hex') + message[(i)*27:(i+1)*27])
   channel.basic_publish(exchange='',
                       routing_key='firstqueue',
                       #routing_key='secondqueue',
-                      body= heading + body,
+                      body= itemtobesent,
                       properties=pika.BasicProperties(delivery_mode = 2,))
-  print("[v] %r is sent" % body)
+  print("[v] %r is sent" % itemtobesent)
+  time.sleep(random.uniform(0,1.5))
+  nextflagraw = nextflagraw ^ xor_message_chunk(itemtobesent[4:])
+  body = message[(i)*27:(i+1)*27]
   i = i + 1
-
-'''
-time.sleep(random.uniform(0,2))
-channel.basic_publish(exchange='',
-                      routing_key='firstqueue',
-                      #routing_key='secondqueue',
-                      body="FFFF" + lenmessage + message[0:int(lenmessage)],
-                      properties=pika.BasicProperties(delivery_mode = 2,))
-'''
-#print("[v] %r is sent" % message)
-
-connection.close()
