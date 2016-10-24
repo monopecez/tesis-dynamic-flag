df0ec56459e94b62de21ea489ffdb512ec40c0e9
Isman Junian Rahman <monopecez@gmail.com>
2016-10-24 18:28:21 +0700

    - send.py ketika panggil send.py tanpa argumen, akan mengirim message yang telah didefinisikan. "send.py 1" maka mengganti spasi di message yang telah didefinisikan menjadi 1 dan "send.py 2" akan mengganti spasi di message menjadi 2   bug fixes

--- send.py
+++ send.py
@@ -37,8 +37,17 @@ print("[v] %r : IV is sent" % IV)
 initialflag = "FFFFFF"
 
 nextflagraw = IV ^ int(initialflag,16)
+message = ' '.join(sys.argv[1:])
 
-message = ' '.join(sys.argv[1:]) or "This page contains examples on basic concepts of C programming like: loops, functions, pointers, structures etc. All the examples in this page are tested and verified on GNU GCC compiler, although almost every program on in this website will work on any compiler you use. Feel free to copy the source code and execute it in your device."
+try:
+  if sys.argv[1] == '1':
+    message = "This1page1contains1examples1on1basic1concepts1of1C1programming1like:1loops,1functions,1pointers,1structures1etc.1All1the1examples1in1this1page1are1tested1and1verified1on1GNU1GCC1compiler,1although1almost1every1program1on1in1this1website1will1work1on1any1compiler1you1use.1Feel1free1to1copy1the1source1code1and1execute1it1in1your1device."
+  elif sys.argv[1] == '2':
+    message = "This2page2contains2examples2on2basic2concepts2of2C2programming2like:2loops,2functions,2pointers,2structures2etc.2All2the2examples2in2this2page2are2tested2and2verified2on2GNU2GCC2compiler,2although2almost2every2program2on2in2this2website2will2work2on2any2compiler2you2use.2Feel2free2to2copy2the2source2code2and2execute2it2in2your2device."
+except:
+  message = "This page contains examples on basic concepts of C programming like: loops, functions, pointers, structures etc. All the examples in this page are tested and verified on GNU GCC compiler, although almost every program on in this website will work on any compiler you use. Feel free to copy the source code and execute it in your device."
+
+
 #message = "This page contains examples on basic concepts" # of C programming like: loops, functions, pointers, structures etc. All the examples in this page are tested and verified on GNU GCC compiler, although almost every program on in this website will work on any compiler you use. Feel free to copy the source code and execute it in your device."
 totalpadding = 32 - (len(message)%32)
 message = message + totalpadding*' '
@@ -58,7 +67,8 @@ while i != len(message)/32:
                       body= itemtobesent,
                       properties=pika.BasicProperties(delivery_mode = 2,))
   print("[v] %s is sent" % itemtobesent)
-  #time.sleep(random.uniform(0,1.5))
-  nextflagraw = nextflagraw ^ xor_message_chunk(itemtobesent[4:])
+  time.sleep(random.uniform(0,1.5))
+  nextflagraw = nextflagraw ^ xor_message_chunk(itemtobesent[3:])
+  #print(nextflagraw)
   body = message[(i)*32:(i+1)*32]
   i = i + 1
